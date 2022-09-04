ed4198b7cbc83c4eda5920c70bd3392559f91f53
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-04 08:22:31 +0200

    has seachable name

--- src/Traits/HasSearchableName.php
+++ src/Traits/HasSearchableName.php
@@ -9,10 +9,17 @@ use ReflectionClass;
  * Your model have to be connected to `laravel/scout` with `Searchable` trait.
  *
  * Create a `searchableAs` method in your model to return `APP_NAME` and Model's name slugify.
+ *
+ * ```php
+ * public function searchableAs()
+ * {
+ *    return $this->searchableNameAs();
+ * }
+ * ```
  */
 trait HasSearchableName
 {
-    public function searchableAs()
+    public function searchableNameAs()
     {
         $instance = new $this();
         $class = new ReflectionClass($instance);
