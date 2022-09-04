103d87058424080d2954ee5833cc24508dc8d00e
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-04 15:29:46 +0200

    add filament widget

---
+++ resources/views/filament/widgets/welcome-info-widget.blade.php
@@ -0,0 +1,5 @@
+<x-filament::widget>
+  <x-filament::card>
+    Welcome on your dashboard!
+  </x-filament::card>
+</x-filament::widget>
---
+++ src/Filament/Widgets/WelcomeInfoWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kiwilan\Steward\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class WelcomeInfoWidget extends Widget
+{
+    protected static string $view = 'steward::filament.widgets.welcome-info-widget';
+}
