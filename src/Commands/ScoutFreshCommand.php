08785835221794143e53b03ffa4627b0da7cb717
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-04 08:20:32 +0200

    scout fresh fix

--- src/Commands/ScoutFreshCommand.php
+++ src/Commands/ScoutFreshCommand.php
@@ -35,22 +35,31 @@ class ScoutFreshCommand extends Command
         $this->warn($this->description);
 
         $list = config('steward.scoutable');
+        $this->info('Models to search engine:'.implode(', ', $list));
+        $this->newLine();
         foreach ($list as $model) {
             $this->getScoutName($model);
         }
 
         try {
+            $this->warn('Clean all models in search engine.');
             foreach ($this->models as $key => $value) {
                 Artisan::call('scout:flush "'.$key.'"', [], $this->getOutput());
                 Artisan::call('scout:delete-index "'.$value.'"', [], $this->getOutput());
             }
+            $this->newLine();
 
+            $this->info('Import all models in search engine.');
             foreach ($this->models as $key => $value) {
                 Artisan::call('scout:import "'.$key.'"', [], $this->getOutput());
             }
+            $this->newLine();
         } catch (\Throwable $th) {
             $this->error($th->getMessage());
         }
+
+        $this->newLine();
+        $this->info('Done.');
 
         return 0;
     }
