e8df2ddc8ab772bd3282f6dd4145ad2225d1f8e6
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-06 21:38:17 +0200

    add query

---
+++ src/Services/QueryService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kiwilan\Steward\Services;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class QueryService
+{
+    public static function boot(Builder $query, string $model, array $config, array $filters)
+    {
+        $service = new QueryService();
+        return $query->where(
+            function (Builder $query) use ($filters, $config, $service) {
+                foreach ($filters as $field => $value) {
+                    $mode = array_key_exists($field, $config) ? $config[$field] : 'exact';
+                    $query = match ($mode) {
+                        'partial' => $service->wherePartial($query, $field, $value),
+                        default => $service->whereExact($query, $field, $value),
+                    };
+                }
+            }
+        );
+    }
+
+    private function wherePartial(Builder $query, string $field, string $value)
+    {
+        return $query->where($field, 'like', "%{$value}%");
+    }
+
+    private function whereExact(Builder $query, string $field, string $value)
+    {
+        return $query->where($field, '=', $value);
+    }
+}
---
+++ src/Traits/Filterable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Kiwilan\Steward\Traits;
+
+use Kiwilan\Steward\Services\QueryService;
+use Illuminate\Database\Eloquent\Builder;
+use ReflectionClass;
+
+trait Filterable
+{
+    // protected $default_role_column = 'role';
+
+    // public function initializeHasRole()
+    // {
+    //     $this->fillable[] = $this->getRoleColumn();
+    //     $this->fillable[] = 'is_blocked';
+
+    //     $this->casts[$this->getRoleColumn()] = UserRoleEnum::class;
+    //     $this->casts['is_blocked'] = 'boolean';
+    // }
+
+    public function scopeFilter(Builder $query, array $filters)
+    {
+        // $queryAll = null;
+        // foreach ($filters as $field => $value) {
+        //     $queryAll[$field] = $query->where($field, $value);
+        // }
+        // // dd($queryAll);
+        // // return $query->where('status', '=', $status);
+        // return $queryAll;
+
+        $queryConfig = [
+            'name' => 'partial',
+            'status' => 'exact',
+        ];
+        // dump($filters);
+
+        // $manual = ModelsMiniature::where(
+        //     function (Builder $query) use ($filters) {
+        //         return $query
+        //             ->where('name', 'like', "%{$filters['name']}%")
+        //             ->where('status', '=', $filters['status'])
+        //             // ->where('price', '>', $filters['min_price'])
+        //         ;
+        //     }
+        // )
+        //     ->get()
+        // ;
+        // dump($manual);
+        $instance = new $this();
+        $class = new ReflectionClass($instance);
+
+        $service = QueryService::boot($query, $class->getName(), $queryConfig, $filters);
+
+        return $service;
+    }
+}
