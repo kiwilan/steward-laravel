0b9a0aecd35db0ef2c0bcf3c1f0087929d4638b3
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-18 10:08:05 +0200

    vite plugin js

---
+++ resources/js/vite-color-mode.js
@@ -0,0 +1,28 @@
+import type { Plugin } from 'vite'
+import fs from 'fs'
+
+const DEFAULT_OPTIONS = {
+  outputDir: './public/vendor/js',
+}
+
+const colorMode = (options = {}) => {
+  return {
+    name: 'vite-color-mode',
+    async buildStart() {
+      const opts = Object.assign({}, DEFAULT_OPTIONS, options)
+
+      const pathColorMode = 'vendor/kiwilan/laravel-steward/resources/js/color-mode.js'
+      const path = process.cwd()
+      const fullPath = `${path}/${pathColorMode}`
+
+      await fs.promises.mkdir(opts.outputDir, { recursive: true }).catch(console.error)
+
+      fs.copyFile(fullPath, `${opts.outputDir}/color-mode.js`, (err) => {
+        if (err)
+          throw err
+      })
+    },
+  }
+}
+
+export default colorMode
