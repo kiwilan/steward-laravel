8d9bd3fb967005ea1f945e9e9eb5e77cc7e6f569
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-18 21:16:52 +0200

    build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   name: 'steward',
   entryPoints: {
-    'vite-steward': 'lib/vite-steward.ts',
-    'tiptap': 'lib/tiptap.ts',
+    steward: 'lib/vite-steward.ts',
+    tiptap: 'lib/tiptap.ts',
   },
   format: ['esm', 'cjs'],
   dts: true,
