60648156f17cfc4f5cd1ca4569c1c1d1e242cb04
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-21 19:52:36 +0200

    wip

--- npm/dist/tiptap.d.ts
+++
@@ -1,5 +0,0 @@
-export { ChainedCommands, Editor } from '@tiptap/core';
-export { default as StarterKit } from '@tiptap/starter-kit';
-export { default as Typography } from '@tiptap/extension-typography';
-export { default as CharacterCount } from '@tiptap/extension-character-count';
-export { default as Link } from '@tiptap/extension-link';
--- npm/src/lib/editor.ts
+++
@@ -1,3 +0,0 @@
-import Editor from './editor-lib'
-
-export default Editor
