1db5b88c5e568850ae18b1c0b94d53fd4b2ac70c
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-10-07 09:14:28 +0200

    format

--- src/Resources/DefaultResource.php
+++ src/Resources/DefaultResource.php
@@ -12,14 +12,13 @@ class DefaultResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-            'error' => 'No resource found'
+            'error' => 'No resource found',
         ];
     }
 }
