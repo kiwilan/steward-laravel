4daf1faf16e911f5de3d7c4ba0ba409b33e1efca
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-10-07 10:06:26 +0200

    queryable add docs

--- src/Traits/Queryable.php
+++ src/Traits/Queryable.php
@@ -4,26 +4,41 @@ namespace Kiwilan\Steward\Traits;
 
 trait Queryable
 {
+    /**
+     * Get Model relationships, default is native `$with`.
+     */
     public function getQueryWith(): array
     {
         return $this->query_with ?? $this->with ?? [];
     }
 
+    /**
+     * Get Model relationships count, default is native `$withCount`.
+     */
     public function getQueryWithCount(): array
     {
-        return $this->query_with_count ?? $this->with_count ?? [];
+        return $this->query_with_count ?? $this->withCount ?? [];
     }
 
+    /**
+     * Get Model default sort field, default is `id` or configured `steward.query.default_sort`.
+     */
     public function getQueryDefaultSort(): string
     {
         return $this->query_default_sort ?? config('steward.query.default_sort');
     }
 
+    /**
+     * Get Model default sort direction, default is `desc` or configured `steward.query.default_sort_direction`.
+     */
     public function getQueryDefaultSortDirection(): string
     {
         return $this->query_default_sort_direction ?? config('steward.query.default_sort_direction');
     }
 
+    /**
+     * Get Model query allowed filters from `setQueryAllowedFilters()` or `$query_allowed_filters`.
+     */
     public function getQueryAllowedFilters(): array
     {
         if (method_exists($this, 'setQueryAllowedFilters')) {
@@ -33,6 +48,9 @@ trait Queryable
         return $this->query_allowed_filters ?? [];
     }
 
+    /**
+     * Get Model query allowed sorts from `setQueryAllowedSorts()` or `$query_allowed_sorts`.
+     */
     public function getQueryAllowedSorts(): array
     {
         if (method_exists($this, 'setQueryAllowedSorts')) {
@@ -42,21 +60,33 @@ trait Queryable
         return $this->query_allowed_sorts ?? [];
     }
 
+    /**
+     * Get Model no pagination directive or configured `steward.query.full`.
+     */
     public function getQueryFull(): bool
     {
         return $this->query_full ?? config('steward.query.full');
     }
 
+    /**
+     * Get Model limit for pagination or configured `steward.query.limit`.
+     */
     public function getQueryLimit(): int
     {
         return $this->query_limit ?? config('steward.query.limit');
     }
 
+    /**
+     * Get Model Export class.
+     */
     public function getQueryExport(): ?string
     {
         return $this->query_export ?? null;
     }
 
+    /**
+     * Get Model Resource class.
+     */
     public function getQueryResource(): ?string
     {
         return $this->query_resource ?? null;
