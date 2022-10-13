2f65a035711d8cb01bab27ade0b7a928166ddbac
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-10-13 20:52:53 +0200

    StwTemplateConfig

---
+++ src/Filament/StwTemplateConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kiwilan\Steward\Filament;
+
+use Filament\Forms;
+
+class StwTemplateConfig
+{
+    public static function block(array $content = [], string $make = 'block', string $label = 'Block')
+    {
+        return Forms\Components\Repeater::make($make)
+            ->schema([
+                StwBuilderConfig::display(),
+                ...$content,
+            ])
+            ->disableItemMovement()
+            ->maxItems(1)
+            ->columnSpan(2)
+            ->label($label)
+            ->createItemButtonLabel("Add {$label}")
+        ;
+    }
+}
