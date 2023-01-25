934e3acb943e92a8d9cc192c88b9a4f19b3ecb63
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-01-25 12:46:14 +0100

    package 0.0.21

--- lib/modules/vue/composables/useInertia.ts
+++ lib/modules/vue/composables/useInertia.ts
@@ -6,12 +6,13 @@ export function useInertia() {
   const options = inertia.options as any | undefined
   const inertiaRouter = options.router
 
+  const convertURL = (url: Route) => `/${url}`
   const router = {
-    get: (url: Route, data?: RequestPayload) => inertiaRouter?.get(url, data),
-    post: (url: Route, data?: RequestPayload) => inertiaRouter?.post(url, data),
-    patch: (url: Route, data?: RequestPayload) => inertiaRouter?.patch(url, data),
-    put: (url: Route, data?: RequestPayload) => inertiaRouter?.put(url, data),
-    delete: (url: Route) => inertiaRouter?.delete(url),
+    get: (url: Route, data?: RequestPayload) => inertiaRouter?.get(convertURL(url), data),
+    post: (url: Route, data?: RequestPayload) => inertiaRouter?.post(convertURL(url), data),
+    patch: (url: Route, data?: RequestPayload) => inertiaRouter?.patch(convertURL(url), data),
+    put: (url: Route, data?: RequestPayload) => inertiaRouter?.put(convertURL(url), data),
+    delete: (url: Route) => inertiaRouter?.delete(convertURL(url)),
   }
 
   return {
