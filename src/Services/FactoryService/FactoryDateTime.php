11d6ddcf549259730c6dc7dce850fb15d61e94e3
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-02-24 11:38:13 +0100

    FactoryDateTime add wrapper for timestamps

--- src/Services/FactoryService/FactoryDateTime.php
+++ src/Services/FactoryService/FactoryDateTime.php
@@ -33,17 +33,30 @@ class FactoryDateTime
         );
     }
 
+    /**
+     * Set timestamps.
+     *
+     * @return array<string,string>
+     */
+    public function setTimestamps(string $minimum = '-20 years'): array
+    {
+        $timestamps = $this->timestamps($minimum);
+
+        return [
+            'created_at' => $timestamps->createdAt,
+            'updated_at' => $timestamps->updatedAt,
+        ];
+    }
+
     private function generateDateTime(string $between, string $format = 'Y-m-d H:i:s'): DateTime
     {
         $date = $this->factory->faker()
-            ->dateTimeBetween($between)
-        ;
+            ->dateTimeBetween($between);
 
         while (! $this->checkDateValidity($date)) {
             $date = $this->factory->faker()
                 ->dateTimeBetween($between)
-                ->format($format)
-            ;
+                ->format($format);
         }
 
         return $date;
