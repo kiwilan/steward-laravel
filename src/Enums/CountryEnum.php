0f5d80fd93eea6da4f1515bf8c51b9939e4d522f
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-09 10:15:45 +0100

    add CountryEnum

---
+++ src/Enums/CountryEnum.php
@@ -0,0 +1,496 @@
+<?php
+
+namespace Kiwilan\Steward\Enums;
+
+use Kiwilan\Steward\Traits\LazyEnum;
+
+enum CountryEnum: string
+{
+    use LazyEnum;
+
+    case af = 'af';
+
+    case ax = 'ax';
+
+    case al = 'al';
+
+    case dz = 'dz';
+
+    case as = 'as';
+
+    case ad = 'ad';
+
+    case ao = 'ao';
+
+    case ai = 'ai';
+
+    case aq = 'aq';
+
+    case ag = 'ag';
+
+    case ar = 'ar';
+
+    case am = 'am';
+
+    case aw = 'aw';
+
+    case au = 'au';
+
+    case at = 'at';
+
+    case az = 'az';
+
+    case bs = 'bs';
+
+    case bh = 'bh';
+
+    case bd = 'bd';
+
+    case bb = 'bb';
+
+    case by = 'by';
+
+    case be = 'be';
+
+    case bz = 'bz';
+
+    case bj = 'bj';
+
+    case bm = 'bm';
+
+    case bt = 'bt';
+
+    case bo = 'bo';
+
+    case ba = 'ba';
+
+    case bw = 'bw';
+
+    case bv = 'bv';
+
+    case br = 'br';
+
+    case io = 'io';
+
+    case bn = 'bn';
+
+    case bg = 'bg';
+
+    case bf = 'bf';
+
+    case bi = 'bi';
+
+    case kh = 'kh';
+
+    case cm = 'cm';
+
+    case ca = 'ca';
+
+    case cv = 'cv';
+
+    case ky = 'ky';
+
+    case cf = 'cf';
+
+    case td = 'td';
+
+    case cl = 'cl';
+
+    case cn = 'cn';
+
+    case cx = 'cx';
+
+    case cc = 'cc';
+
+    case co = 'co';
+
+    case km = 'km';
+
+    case cg = 'cg';
+
+    case cd = 'cd';
+
+    case ck = 'ck';
+
+    case cr = 'cr';
+
+    case ci = 'ci';
+
+    case hr = 'hr';
+
+    case cu = 'cu';
+
+    case cy = 'cy';
+
+    case cz = 'cz';
+
+    case dk = 'dk';
+
+    case dj = 'dj';
+
+    case dm = 'dm';
+
+    case do = 'do';
+
+    case ec = 'ec';
+
+    case eg = 'eg';
+
+    case sv = 'sv';
+
+    case gq = 'gq';
+
+    case er = 'er';
+
+    case ee = 'ee';
+
+    case et = 'et';
+
+    case fk = 'fk';
+
+    case fo = 'fo';
+
+    case fj = 'fj';
+
+    case fi = 'fi';
+
+    case fr = 'fr';
+
+    case gf = 'gf';
+
+    case pf = 'pf';
+
+    case tf = 'tf';
+
+    case ga = 'ga';
+
+    case gm = 'gm';
+
+    case ge = 'ge';
+
+    case de = 'de';
+
+    case gh = 'gh';
+
+    case gi = 'gi';
+
+    case gr = 'gr';
+
+    case gl = 'gl';
+
+    case gd = 'gd';
+
+    case gp = 'gp';
+
+    case gu = 'gu';
+
+    case gt = 'gt';
+
+    case gg = 'gg';
+
+    case gn = 'gn';
+
+    case gw = 'gw';
+
+    case gy = 'gy';
+
+    case ht = 'ht';
+
+    case hm = 'hm';
+
+    case va = 'va';
+
+    case hn = 'hn';
+
+    case hk = 'hk';
+
+    case hu = 'hu';
+
+    case is = 'is';
+
+    case in = 'in';
+
+    case id = 'id';
+
+    case ir = 'ir';
+
+    case iq = 'iq';
+
+    case ie = 'ie';
+
+    case im = 'im';
+
+    case il = 'il';
+
+    case it = 'it';
+
+    case jm = 'jm';
+
+    case jp = 'jp';
+
+    case je = 'je';
+
+    case jo = 'jo';
+
+    case kz = 'kz';
+
+    case ke = 'ke';
+
+    case ki = 'ki';
+
+    case kp = 'kp';
+
+    case kr = 'kr';
+
+    case kw = 'kw';
+
+    case kg = 'kg';
+
+    case la = 'la';
+
+    case lv = 'lv';
+
+    case lb = 'lb';
+
+    case ls = 'ls';
+
+    case lr = 'lr';
+
+    case ly = 'ly';
+
+    case li = 'li';
+
+    case lt = 'lt';
+
+    case lu = 'lu';
+
+    case mo = 'mo';
+
+    case mk = 'mk';
+
+    case mg = 'mg';
+
+    case mw = 'mw';
+
+    case my = 'my';
+
+    case mv = 'mv';
+
+    case ml = 'ml';
+
+    case mt = 'mt';
+
+    case mh = 'mh';
+
+    case mq = 'mq';
+
+    case mr = 'mr';
+
+    case mu = 'mu';
+
+    case yt = 'yt';
+
+    case mx = 'mx';
+
+    case fm = 'fm';
+
+    case md = 'md';
+
+    case mc = 'mc';
+
+    case mn = 'mn';
+
+    case ms = 'ms';
+
+    case ma = 'ma';
+
+    case mz = 'mz';
+
+    case mm = 'mm';
+
+    case na = 'na';
+
+    case nr = 'nr';
+
+    case np = 'np';
+
+    case nl = 'nl';
+
+    case an = 'an';
+
+    case nc = 'nc';
+
+    case nz = 'nz';
+
+    case ni = 'ni';
+
+    case ne = 'ne';
+
+    case ng = 'ng';
+
+    case nu = 'nu';
+
+    case nf = 'nf';
+
+    case mp = 'mp';
+
+    case no = 'no';
+
+    case om = 'om';
+
+    case pk = 'pk';
+
+    case pw = 'pw';
+
+    case ps = 'ps';
+
+    case pa = 'pa';
+
+    case pg = 'pg';
+
+    case py = 'py';
+
+    case pe = 'pe';
+
+    case ph = 'ph';
+
+    case pn = 'pn';
+
+    case pl = 'pl';
+
+    case pt = 'pt';
+
+    case pr = 'pr';
+
+    case qa = 'qa';
+
+    case re = 're';
+
+    case ro = 'ro';
+
+    case ru = 'ru';
+
+    case rw = 'rw';
+
+    case sh = 'sh';
+
+    case kn = 'kn';
+
+    case lc = 'lc';
+
+    case pm = 'pm';
+
+    case vc = 'vc';
+
+    case ws = 'ws';
+
+    case sm = 'sm';
+
+    case st = 'st';
+
+    case sa = 'sa';
+
+    case sn = 'sn';
+
+    case cs = 'cs';
+
+    case sc = 'sc';
+
+    case sl = 'sl';
+
+    case sg = 'sg';
+
+    case sk = 'sk';
+
+    case si = 'si';
+
+    case sb = 'sb';
+
+    case so = 'so';
+
+    case za = 'za';
+
+    case gs = 'gs';
+
+    case es = 'es';
+
+    case lk = 'lk';
+
+    case sd = 'sd';
+
+    case sr = 'sr';
+
+    case sj = 'sj';
+
+    case sz = 'sz';
+
+    case se = 'se';
+
+    case ch = 'ch';
+
+    case sy = 'sy';
+
+    case tw = 'tw';
+
+    case tj = 'tj';
+
+    case tz = 'tz';
+
+    case th = 'th';
+
+    case tl = 'tl';
+
+    case tg = 'tg';
+
+    case tk = 'tk';
+
+    case to = 'to';
+
+    case tt = 'tt';
+
+    case tn = 'tn';
+
+    case tr = 'tr';
+
+    case tm = 'tm';
+
+    case tc = 'tc';
+
+    case tv = 'tv';
+
+    case ug = 'ug';
+
+    case ua = 'ua';
+
+    case ae = 'ae';
+
+    case gb = 'gb';
+
+    case us = 'us';
+
+    case um = 'um';
+
+    case uy = 'uy';
+
+    case uz = 'uz';
+
+    case vu = 'vu';
+
+    case ve = 've';
+
+    case vn = 'vn';
+
+    case vg = 'vg';
+
+    case vi = 'vi';
+
+    case wf = 'wf';
+
+    case eh = 'eh';
+
+    case ye = 'ye';
+
+    case zm = 'zm';
+
+    case zw = 'zw';
+}
