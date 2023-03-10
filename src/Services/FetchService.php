84dbe72679d8799800cbd77894a4f8d9235b9a89
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-10 17:37:47 +0100

    FetchService improve

--- src/Services/FetchService.php
+++ src/Services/FetchService.php
@@ -3,6 +3,7 @@
 namespace Kiwilan\Steward\Services;
 
 use GuzzleHttp\Client;
+use Kiwilan\Steward\Utils\Console;
 use Psr\Http\Message\ResponseInterface;
 
 class FetchService
@@ -22,8 +23,19 @@ class FetchService
     {
         $client = new \GuzzleHttp\Client();
 
+        $console = Console::make();
         $self = new self($url, $client, $method);
+
+        $start_time = microtime(true);
+        $domain = parse_url($self->url, PHP_URL_HOST);
+        $console->newLine();
+        $console->print("  Fetching {$domain}...", 'yellow');
         $self->response = $client->request($method, $url);
+
+        $end_time = microtime(true);
+        $execution_time = ($end_time - $start_time);
+        $execution_time = number_format((float) $execution_time, 2, '.', '');
+        $console->print("  Done in {$execution_time} seconds.", 'green');
 
         return $self;
     }
@@ -51,5 +63,10 @@ class FetchService
     public function json(): array
     {
         return json_decode($this->body(), true);
+    }
+
+    public function xml(): array
+    {
+        return json_decode(json_encode(simplexml_load_string($this->body())), true);
     }
 }
