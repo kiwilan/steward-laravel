5b844a3be3dfdef203112d8a25e794e423eb3576
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-06-30 10:11:57 +0200

    update GuzzleOptions timeout 60

--- src/Services/Http/Utils/GuzzleOptions.php
+++ src/Services/Http/Utils/GuzzleOptions.php
@@ -9,7 +9,7 @@ class GuzzleOptions
         public int $poolLimit = 250,
         public int $maxCurlHandles = 100,
         public int $maxRedirects = 10,
-        public int $timeout = 30,
+        public int $timeout = 60,
         public int $guzzleConcurrency = 5,
     ) {
     }
