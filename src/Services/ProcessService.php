4ed5c6c5b54a47407964a1a5cd9e6601b9214df5
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-06 17:51:23 +0200

    ProcessService add memoryPeek

--- src/Services/ProcessService.php
+++ src/Services/ProcessService.php
@@ -21,4 +21,29 @@ class ProcessService
         $console = Console::make();
         $console->print("Execution time of script = {$executionTime} sec", 'green');
     }
+
+    public static function memoryPeekFile(Closure $closure, ?string $path = null, int $limit = 200): void
+    {
+        $currentMemory = ini_get('memory_limit');
+        $filesize = filesize($path);
+        $filesizeMB = $filesize / 1048576;
+        $limit = intval($filesizeMB + $limit);
+
+        if (intval($limit) > intval($currentMemory)) {
+            ini_set('memory_limit', $limit.'M');
+        }
+
+        $closure();
+
+        ini_restore('memory_limit');
+    }
+
+    public static function memoryPeek(Closure $closure, int $maxMemory = 10, string $unit = 'G'): void
+    {
+        ini_set('memory_limit', "{$maxMemory}{$unit}");
+
+        $closure();
+
+        ini_restore('memory_limit');
+    }
 }
