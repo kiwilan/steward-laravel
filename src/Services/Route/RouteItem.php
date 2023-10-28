2a4a4418b41794b6ba654d30d2758d7325266e53
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-10-28 19:39:09 +0200

    route

---
+++ src/Services/Route/RouteItem.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Kiwilan\Steward\Services\Route;
+
+class RouteItem
+{
+    /**
+     * @param  string[]  $methods
+     * @param  string[]  $middleware
+     * @param  string[]  $parameters
+     */
+    public function __construct(
+        protected ?string $name = null,
+        protected ?array $methods = null,
+        protected ?string $uri = null,
+        protected ?string $action = null,
+        protected ?array $middleware = null,
+        protected ?string $example = null,
+        protected ?array $parameters = null,
+    ) {
+    }
+
+    public function name(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function methods(): ?array
+    {
+        return $this->methods;
+    }
+
+    public function uri(): ?string
+    {
+        return $this->uri;
+    }
+
+    public function action(): ?string
+    {
+        return $this->action;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function middleware(): ?array
+    {
+        return $this->middleware;
+    }
+
+    public function example(): ?string
+    {
+        return $this->example;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function parameters(): ?array
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'methods' => $this->methods,
+            'uri' => $this->uri,
+            'action' => $this->action,
+            'middleware' => $this->middleware,
+            'example' => $this->example,
+            'parameters' => $this->parameters,
+        ];
+    }
+}
--- src/Services/RouteService.php
+++ src/Services/RouteService.php
@@ -5,6 +5,7 @@ namespace Kiwilan\Steward\Services;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route as FacadesRoute;
+use Kiwilan\Steward\Services\Route\RouteItem;
 
 class RouteService
 {
@@ -101,84 +102,5 @@ class RouteService
             example: config('app.url')."/{$uri}",
             parameters: $route->parameterNames(),
         );
-    }
-}
-
-class RouteItem
-{
-    /**
-     * @param  string[]  $methods
-     * @param  string[]  $middleware
-     * @param  string[]  $parameters
-     */
-    public function __construct(
-        protected ?string $name = null,
-        protected ?array $methods = null,
-        protected ?string $uri = null,
-        protected ?string $action = null,
-        protected ?array $middleware = null,
-        protected ?string $example = null,
-        protected ?array $parameters = null,
-    ) {
-    }
-
-    public function name(): ?string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function methods(): ?array
-    {
-        return $this->methods;
-    }
-
-    public function uri(): ?string
-    {
-        return $this->uri;
-    }
-
-    public function action(): ?string
-    {
-        return $this->action;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function middleware(): ?array
-    {
-        return $this->middleware;
-    }
-
-    public function example(): ?string
-    {
-        return $this->example;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function parameters(): ?array
-    {
-        return $this->parameters;
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    public function toArray(): array
-    {
-        return [
-            'name' => $this->name,
-            'methods' => $this->methods,
-            'uri' => $this->uri,
-            'action' => $this->action,
-            'middleware' => $this->middleware,
-            'example' => $this->example,
-            'parameters' => $this->parameters,
-        ];
     }
 }
