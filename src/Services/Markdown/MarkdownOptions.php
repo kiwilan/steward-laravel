568f38f97451e66d4db0a13885152953bde40860
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-11-27 19:48:16 +0100

    Refactor MarkdownService getConfig method

--- src/Services/Markdown/MarkdownOptions.php
+++ src/Services/Markdown/MarkdownOptions.php
@@ -2,6 +2,8 @@
 
 namespace Kiwilan\Steward\Services\Markdown;
 
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+
 class MarkdownOptions
 {
     public function __construct(
@@ -10,7 +12,111 @@ class MarkdownOptions
             'APP_URL' => 'app.url',
         ],
         protected ?string $imagePath = null,
+        protected array $config = [],
+        protected bool $mergeConfig = true,
     ) {
+        $defaultConfig = [
+            'html_input' => 'strip',
+            'allow_unsafe_links' => false,
+            'external_link' => [
+                'internal_hosts' => config('app.url'),
+                'open_in_new_window' => true,
+                'html_class' => 'external-link',
+                'nofollow' => '',
+                'noopener' => 'external',
+                'noreferrer' => 'external',
+            ],
+            'default_attributes' => [
+                Heading::class => [
+                    'class' => static function (Heading $node) {
+                        if ($node->getLevel() === 1) {
+                            return 'title-main';
+                        }
+                    },
+                ],
+                Table::class => [
+                    'class' => 'table',
+                ],
+                Paragraph::class => [
+                    'class' => ['word-wraping', 'font-sans'],
+                ],
+                Link::class => [
+                    'class' => 'btn btn-link',
+                    'target' => '_blank',
+                ],
+            ],
+            'footnote' => [
+                'backref_class' => 'footnote-backref',
+                'backref_symbol' => '↩',
+                'container_add_hr' => true,
+                'container_class' => 'footnotes',
+                'ref_class' => 'footnote-ref',
+                'ref_id_prefix' => 'fnref:',
+                'footnote_class' => 'footnote',
+                'footnote_id_prefix' => 'fn:',
+            ],
+            'heading_permalink' => [
+                'html_class' => 'heading-permalink',
+                'id_prefix' => 'content',
+                'fragment_prefix' => 'content',
+                'insert' => 'before',
+                'min_heading_level' => 1,
+                'max_heading_level' => 6,
+                'title' => 'Permalink',
+                'symbol' => '',
+            ],
+            'mentions' => [
+                // GitHub handler mention configuration.
+                // Sample Input:  `@colinodell`
+                // Sample Output: `<a href="https://www.github.com/colinodell">@colinodell</a>`
+                'github_handle' => [
+                    'prefix' => '@',
+                    'pattern' => '[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)',
+                    'generator' => 'https://github.com/%s',
+                ],
+                // GitHub issue mention configuration.
+                // Sample Input:  `#473`
+                // Sample Output: `<a href="https://github.com/thephpleague/commonmark/issues/473">#473</a>`
+                'github_issue' => [
+                    'prefix' => '#',
+                    'pattern' => '\d+',
+                    'generator' => 'https://github.com/thephpleague/commonmark/issues/%d',
+                ],
+                // Twitter handler mention configuration.
+                // Sample Input:  `@colinodell`
+                // Sample Output: `<a href="https://www.twitter.com/colinodell">@colinodell</a>`
+                // Note: when registering more than one mention parser with the same prefix, the first mention parser to
+                // successfully match and return a properly constructed Mention object (where the URL has been set) will be the
+                // the mention parser that is used. In this example, the GitHub handle would actually match first because
+                // there isn't any real validation to check whether https://www.github.com/colinodell exists. However, in
+                // CMS applications, you could check whether its a local user first, then check Twitter and then GitHub, etc.
+                'twitter_handle' => [
+                    'prefix' => '@',
+                    'pattern' => '[A-Za-z0-9_]{1,15}(?!\w)',
+                    'generator' => 'https://twitter.com/%s',
+                ],
+            ],
+            'smartpunct' => [
+                'double_quote_opener' => '“',
+                'double_quote_closer' => '”',
+                'single_quote_opener' => '‘',
+                'single_quote_closer' => '’',
+            ],
+            // 'table_of_contents' => [
+            //     'html_class' => 'table-of-contents',
+            //     'position' => 'top',
+            //     'style' => 'bullet',
+            //     'min_heading_level' => 1,
+            //     'max_heading_level' => 6,
+            //     'normalize' => 'relative',
+            //     'placeholder' => null,
+            // ],
+        ];
+        if (empty($this->config)) {
+            $this->config = $defaultConfig;
+        } elseif ($this->mergeConfig) {
+            $this->config = array_merge($defaultConfig, $this->config);
+        }
     }
 
     /**
@@ -24,5 +130,13 @@ class MarkdownOptions
     public function imagesPath(): ?string
     {
         return $this->imagePath;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function config(): array
+    {
+        return $this->config;
     }
 }
