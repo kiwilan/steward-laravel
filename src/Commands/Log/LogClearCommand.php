7919ef93e640508a7f5888fc579570fc2a03d29e
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-12-20 11:49:51 +0100

    Refactor log clearing functionality

--- src/Commands/Log/LogClearCommand.php
+++ src/Commands/Log/LogClearCommand.php
@@ -12,21 +12,24 @@ class LogClearCommand extends Commandable
      *
      * @var string
      */
-    protected $signature = 'log:clear {name? : name of log file}
-                            {--a|all : clear all log files}';
+    protected $signature = 'log:clear
+                            {--a|all : clear all log files}
+                            {--n|name= : clear specific log file}
+                            {--l|level= : clear level log like `DEBUG`, `INFO`, `NOTICE`, `WARNING`, `ERROR`, `CRITICAL`, `ALERT`, `EMERGENCY`}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Clear specific log';
+    protected $description = 'Clear logs.';
 
     /**
      * Create a new command instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public ?string $path = null,
+    ) {
         parent::__construct();
     }
 
@@ -38,22 +41,76 @@ class LogClearCommand extends Commandable
     public function handle()
     {
         $this->title();
+        $this->path = storage_path('logs/laravel.log');
 
-        $all = $this->option('all') ?: false;
-        $log_name = $this->argument('name');
+        $all = $this->option('all') ?? false;
+        $log_name = $this->option('name') ?? null;
+        $level = $this->option('level') ?? null;
 
-        if (! $log_name && ! $all) {
+        if (! $log_name && ! $all && ! $level) {
             $all = true;
         }
 
+        if ($all) {
+            $this->info('Clearing all logs...');
+            $this->clearAll();
+        }
+
         if ($log_name) {
-            shell_exec("truncate -s 0 ./storage/logs/{$log_name}.log");
-        } elseif ($all) {
-            shell_exec('truncate -s 0 ./storage/logs/*.log');
-        } else {
-            $this->error('You must specify a log name or use --all option');
+            $this->info("Clearing {$log_name} log...");
+            $this->clearLog($log_name);
+        }
+
+        if ($level) {
+            $this->info("Clearing level {$level} in logs...");
+            $this->deleteLevels($level);
         }
 
         return Command::SUCCESS;
+    }
+
+    private function clearLog(string $log_name): void
+    {
+        shell_exec("truncate -s 0 ./storage/logs/{$log_name}.log");
+    }
+
+    private function clearAll(): void
+    {
+        shell_exec('truncate -s 0 ./storage/logs/*.log');
+    }
+
+    private function deleteLevels(string $level): void
+    {
+        $allLogFiles = glob(storage_path('logs/*.log'));
+
+        foreach ($allLogFiles as $logFile) {
+            $this->info("Clearing {$logFile} for level {$level} log...");
+            $this->deleteLevel($logFile, $level);
+        }
+    }
+
+    private function deleteLevel(string $path, string $level): void
+    {
+        $tempFile = storage_path('logs/temp.log');
+
+        $handle = fopen($path, 'r');
+        $tempHandle = fopen($tempFile, 'w');
+
+        if ($handle && $tempHandle) {
+            while (($line = fgets($handle)) !== false) {
+                if (strpos($line, "local.$level") === false) {
+                    fwrite($tempHandle, $line);
+                }
+            }
+
+            fclose($handle);
+            fclose($tempHandle);
+
+            // Replace original file with temp file
+            rename($tempFile, $path);
+        } else {
+            // Error handling
+            echo 'Error opening file!';
+        }
     }
 }
