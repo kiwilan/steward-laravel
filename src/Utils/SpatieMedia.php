d98305d03c24b69f518d0cab849836d95a026cf6
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-01-16 21:27:13 +0100

    Refactor SpatieMedia class to support different methods for adding media files

--- src/Utils/SpatieMedia.php
+++ src/Utils/SpatieMedia.php
@@ -4,115 +4,195 @@ namespace Kiwilan\Steward\Utils;
 
 use Illuminate\Database\Eloquent\Model;
 use Kiwilan\Steward\Enums\SpatieMediaMethodEnum;
-use UnitEnum;
 
 /**
- * MediaService to manage media files.
- *
- * @property Model $model
- * @property string $name
- * @property string|UnitEnum $disk
- * @property ?string $collection
- * @property ?string $extension
- * @property ?SpatieMediaMethodEnum $method
+ * SpatieMedia to manage media files with `spatie/laravel-medialibrary`.
  */
 class SpatieMedia
 {
     protected function __construct(
         protected Model $model,
-        protected string $name,
-        protected mixed $disk,
-        protected ?string $collection = null,
+        protected mixed $data = null,
+        protected ?string $name = null,
+        protected string $disk = 'public',
+        protected bool $color = false,
+        protected string $collection = 'default',
         protected ?string $extension = null,
         protected ?SpatieMediaMethodEnum $method = null,
+        protected array $allowedMimeTypes = []
     ) {
     }
 
     /**
      * Add a media file to the model.
-     *
-     * @param  ?string  $disk - Default is `disks.public.root`.
-     * @param  ?string  $collection - Default is `library`, which is a subfolder of `disk`.
-     * @param  ?string  $extension - Default is `webp`, can be overridden by `config('bookshelves.cover_extension')`.
-     * @param  ?SpatieMediaMethodEnum  $method - Default is `addMediaFromBase64`.
      */
-    public static function make(
-        Model $model,
-        string $name,
-        ?string $disk = null,
-        ?string $collection = null,
-        ?string $extension = null,
-        ?SpatieMediaMethodEnum $method = null
-    ): self {
-        if (! $disk) {
-            $disk = config('filesystems.disks.public.root');
-
-            if (! file_exists($disk)) {
-                mkdir($disk, 0775, true);
-            }
+    public static function make(Model $model): self
+    {
+        if (! \Composer\InstalledVersions::isInstalled('spatie/laravel-medialibrary')) {
+            throw new \Exception('Package spatie/laravel-medialibrary not installed, see https://spatie.be/docs/laravel-medialibrary');
         }
 
-        if (! $collection) {
-            $collection = 'library';
-        }
-
-        if (! $extension) {
-            $extension = config('bookshelves.cover_extension');
-        }
-
-        if (! $method) {
-            $method = SpatieMediaMethodEnum::addMediaFromBase64;
-        }
-
-        $path = $disk.'/'.$collection;
-
-        if (! file_exists($path)) {
-            mkdir($path, 0775, true);
-        }
-
-        return new self($model, $name, $disk, $collection, $extension, $method);
+        return new self($model);
     }
 
-    public function addMedia(...$data): self
+    public function addMedia(string|\Symfony\Component\HttpFoundation\File\UploadedFile $file): self
     {
-        if ($data) {
-            $this->model->{$this->method->value}($data)
-                ->setName($this->name)
-                ->setFileName($this->name.'.'.$this->extension)
-                ->toMediaCollection($this->collection, $this->disk)
-            ;
-            $this->model->refresh();
-        }
+        $this->method = SpatieMediaMethodEnum::addMedia;
+        $this->data = $file;
 
         return $this;
     }
 
-    public function setColor(): self
+    public function addMediaFromBase64(string $base64): self
     {
-        // @phpstan-ignore-next-line
-        $image = $this->model->getFirstMediaPath($this->collection);
+        $this->method = SpatieMediaMethodEnum::addMediaFromBase64;
+        $this->data = $base64;
 
-        if ($image) {
-            $color = Picture::color($image);
-            // @phpstan-ignore-next-line
-            $media = $this->model->getFirstMedia($this->collection);
+        return $this;
+    }
+
+    public function addMediaFromDisk(string $path): self
+    {
+        $this->method = SpatieMediaMethodEnum::addMediaFromDisk;
+        $this->data = $path;
+
+        return $this;
+    }
+
+    public function addMediaFromRequest(string $key): self
+    {
+        $this->method = SpatieMediaMethodEnum::addMediaFromRequest;
+        $this->data = $key;
+
+        return $this;
+    }
+
+    /**
+     * @param  resource  $resource
+     */
+    public function addMediaFromStream($resource): self
+    {
+        $this->method = SpatieMediaMethodEnum::addMediaFromStream;
+        $this->data = $resource;
+
+        return $this;
+    }
+
+    public function addMediaFromString(string $data): self
+    {
+        $this->method = SpatieMediaMethodEnum::addMediaFromString;
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function addMediaFromUrl(string $url): self
+    {
+        $this->method = SpatieMediaMethodEnum::addMediaFromUrl;
+        $this->data = $url;
+
+        return $this;
+    }
+
+    public function name(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function extension(string $extension): self
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    /**
+     * For `addMediaFromBase64` and `addMediaFromUrl`.
+     */
+    public function allowedMimeTypes(array $allowedMimeTypes): self
+    {
+        $this->allowedMimeTypes = $allowedMimeTypes;
+
+        return $this;
+    }
+
+    public function collection(string $collection): self
+    {
+        $this->collection = $collection;
+
+        return $this;
+    }
+
+    public function disk(string $disk): self
+    {
+        $this->disk = $disk;
+
+        return $this;
+    }
+
+    public function color(): self
+    {
+        $this->color = true;
+
+        return $this;
+    }
+
+    public function save(): void
+    {
+        $spatie = $this->model->{$this->method->value}($this->data);
+
+        if ($this->method === SpatieMediaMethodEnum::addMediaFromBase64 || $this->method === SpatieMediaMethodEnum::addMediaFromUrl) {
+            $spatie = $this->model->{$this->method->value}($this->data, $this->allowedMimeTypes);
+        }
+
+        if (! $this->name) {
+            $this->name = uniqid();
+        }
+
+        if (! $this->extension) {
+            $this->extension = 'jpg';
+        }
+
+        if (! $this->collection) {
+            $this->collection = '';
+        }
+
+        if (! $this->disk) {
+            $this->disk = '';
+        }
+
+        $media = $spatie->usingName($this->name)
+            ->usingFileName("{$this->name}.{$this->extension}")
+            ->toMediaCollection($this->collection, $this->disk)
+        ;
+
+        if ($this->color) {
+            $this->model->refresh();
+            $color = Picture::color($media->getPath());
             $media->setCustomProperty('color', $color);
             $media->save();
         }
-
-        return $this;
     }
 
     public static function getFullUrl(Model $model, string $collection, ?string $conversion = ''): string
     {
-        $cover = null;
+        $defaultUrl = config('app.url').'/vendor/vendor/images/no-cover.webp';
+
+        if (! method_exists($model, 'getFirstMediaUrl')) {
+            return $defaultUrl;
+        }
 
         try {
-            // @phpstan-ignore-next-line
             $cover = $model->getFirstMediaUrl($collection, $conversion);
+
+            if ($cover) {
+                return $cover;
+            }
         } catch (\Throwable $th) {
         }
 
-        return $cover ? $cover : config('app.url').'/vendor/vendor/images/no-cover.webp';
+        return $defaultUrl;
     }
 }
