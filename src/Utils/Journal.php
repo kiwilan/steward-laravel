35c3750360bf74bd48caf697f22edc64b8993bc0
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-01-27 09:17:28 +0100

    Update exception handling in Journal class

--- src/Utils/Journal.php
+++ src/Utils/Journal.php
@@ -42,7 +42,7 @@ class Journal
     /**
      * Handle exception, log as error and send notification to database.
      *
-     * In local environment, it will return null, to not send notification to database.
+     * In `local` environment, it will return null, to not send notification to database.
      */
     public static function handler(\Throwable $e): ?self
     {
