459307b3bf618833b7dbecbca15cb02625645b23
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-15 14:16:36 +0100

    Refactor MetaTags component to use props and add conditional rendering of meta tags

--- resources/views/components/meta-tags.blade.php
+++ resources/views/components/meta-tags.blade.php
@@ -1,54 +1,71 @@
-<!-- HTML Meta Tags -->
-<title>Default Title</title>
-<meta
-  name="description"
-  content="Default Description"
->
+@if ($title)
+  <meta
+    property="og:title"
+    content="{{ $title }}"
+  >
+  <meta
+    name="twitter:title"
+    content="{{ $title }}"
+  >
+@endif
 
-<!-- Facebook Meta Tags -->
-<meta
-  property="og:url"
-  content="{{ config('app.url') }}"
->
+@if ($description)
+  <meta
+    name="description"
+    content="{{ $description }}"
+  >
+  <meta
+    property="og:description"
+    content="{{ $description }}"
+  >
+  <meta
+    name="twitter:description"
+    content="{{ $description }}"
+  >
+@endif
+
+@if ($author)
+  <meta
+    name="author"
+    content="{{ $author }}"
+  />
+  <meta
+    name="twitter:creator"
+    content="{{ $author }}"
+  />
+@endif
+
+@if ($url)
+  <meta
+    property="og:url"
+    content="{{ $url }}"
+  >
+  <meta
+    property="twitter:url"
+    content="{{ $url }}"
+  >
+  <meta
+    property="twitter:domain"
+    content="{{ $domain }}"
+  >
+@endif
+
+@if ($image)
+  <meta
+    property="og:image"
+    content="{{ $image }}"
+  >
+  <meta
+    name="twitter:image"
+    content="{{ $image }}"
+  >
+@endif
+
 <meta
   property="og:type"
   content="website"
 >
 <meta
-  property="og:title"
-  content="Default Title"
->
-<meta
-  property="og:description"
-  content="Default Description"
->
-<meta
-  property="og:image"
-  content="{{ config('app.url') . '/default.jpg' }}"
->
-
-<!-- Twitter Meta Tags -->
-<meta
   name="twitter:card"
-  content="summary_large_image"
->
-<meta
-  property="twitter:domain"
-  content="{{ config('app.url') }}"
->
-<meta
-  property="twitter:url"
-  content="{{ config('app.url') }}"
->
-<meta
-  name="twitter:title"
-  content="Default Title"
->
-<meta
-  name="twitter:description"
-  content="Default Description"
->
-<meta
-  name="twitter:image"
-  content="{{ config('app.url') . '/default.jpg' }}"
+  content="{{ $twitter }}"
 >
--- src/Components/MetaTags.php
+++ src/Components/MetaTags.php
@@ -10,6 +10,15 @@ class MetaTags extends Component
      * Create a new component instance.
      */
     public function __construct(
+        public ?array $props = null,
+        public string $key = 'meta',
+        public string $twitter = 'summary_large_image', // summary, summary_large_image, app, player
+        public ?string $title = null,
+        public ?string $description = null,
+        public ?string $image = null,
+        public ?string $url = null,
+        public ?string $domain = null,
+        public ?string $author = null,
     ) {
     }
 
@@ -20,6 +29,41 @@ class MetaTags extends Component
      */
     public function render()
     {
+        if (empty($this->props)) {
+            $this->props = [
+                $this->key => [],
+            ];
+        }
+
+        if (empty($this->props[$this->key])) {
+            $this->props[$this->key] = [
+                'title' => config('app.name'),
+                'description' => null,
+                'image' => config('app.url').'/default.jpg',
+            ];
+        }
+
+        $this->title = $this->title ?? $this->props[$this->key]['title'];
+        $this->description = $this->description ?? $this->props[$this->key]['description'];
+        $this->image = $this->image ?? $this->props[$this->key]['image'];
+
+        if (strlen($this->title) > 70) {
+            $this->title = substr($this->title, 0, 70).'...';
+        }
+
+        if (strlen($this->description) > 200) {
+            $this->description = substr($this->description, 0, 200).'...';
+        }
+
+        if (! $this->author) {
+            $this->author = config('app.name');
+        }
+
+        if (url()->current()) {
+            $this->url = $this->url ?? url()->current();
+            $this->domain = $this->domain ?? parse_url($this->url, PHP_URL_HOST);
+        }
+
         return view('steward::components.meta-tags');
     }
 }
