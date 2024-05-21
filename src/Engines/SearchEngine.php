3b16bec01bb106f981b2e92916907cf4f3646910
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-05-21 15:42:20 +0200

    improve SearchEngine

--- src/Engines/SearchEngine.php
+++ src/Engines/SearchEngine.php
@@ -2,145 +2,216 @@
 
 namespace Kiwilan\Steward\Engines;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
-use Kiwilan\Steward\Services\ClassParser\ClassParserItem;
-use Kiwilan\Steward\Services\ClassParserService;
+use ReflectionClass;
 
 /**
  * Search Engine with laravel/scout
- * - https://laravel.com/docs/10.x/scout.
+ * - https://laravel.com/docs/11.x/scout.
  */
 class SearchEngine
 {
-    public array $results = [];
-
-    public array $results_relevant = [];
-
-    public function __construct(
-        public int $top_limit = 3,
-        public int $max_limit = 10,
-        public int $count = 0,
-        public string $search_type = 'collection',
-        public ?string $q = null,
-        public ?bool $relevant = false,
-        public ?bool $opds = false,
-        public ?array $types = [],
+    /**
+     * @param  string|null  $query  The search query. Example: "lord of".
+     * @param  string[]  $scoutable  The list of scoutable models. Example: [\App\Models\Book::class, \App\Models\Serie::class, \App\Models\Author::class].
+     * @param  int  $limit  The limit of results.
+     * @param  int  $count  The count of results.
+     * @param  Collection<string, Collection<Model>>|null  $results  The results.
+     * @param  string|null  $resource  The `Illuminate\Http\Resources\Json\JsonResource` resource.
+     */
+    protected function __construct(
+        protected ?string $query = null,
+        protected array $scoutable = [],
+        protected ?int $limit = null,
+        protected int $count = 0,
+        protected ?Collection $results = null,
+        protected ?string $resource = null,
     ) {
     }
 
     /**
-     * Create an instance of SearchEngine from query.
-     */
-    public static function create(?string $q = '', bool $relevant = false, bool $opds = false, string|array|null $types = null): SearchEngine
-    {
-        if (gettype($types) === 'string') {
-            $types = explode(',', $types);
-        }
-
-        if (empty($types)) {
-            $types = [
-                'authors',
-                'series',
-                'books',
-            ];
-        }
-        $engine = new SearchEngine(q: $q, relevant: $relevant, opds: $opds, types: $types);
-
-        return $engine->searchEngine();
-    }
-
-    public function json(): JsonResponse
-    {
-        return response()->json([
-            'data' => [
-                'count' => empty($this->q) ? 0 : $this->count,
-                'type' => $this->search_type,
-                'query' => $this->q,
-                'results' => empty($this->q) ? [] : (! $this->relevant ? $this->results : []),
-                'results_relevant' => empty($this->q) ? [] : (! $this->relevant ? [] : $this->results_relevant),
-            ],
-        ]);
-    }
-
-    /**
-     * Find search engine from laravel/scout and execute search.
-     */
-    public function searchEngine(): SearchEngine
-    {
-        $this->search_type = config('scout.driver');
-        $this->search();
-
-        /** @var Collection $collection */
-        foreach ($this->results as $key => $collection) {
-            $this->count += $collection->count();
-        }
-
-        if ($this->relevant) {
-            $this->results_relevant = $this->getRelevantResults();
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get relevant results.
+     * Search for models.
      *
-     * @return array<string,array<string,array<string,mixed>>>
+     * @param  string  $query  The search query. Example: `lord of`.
+     * @param  string[]  $scoutableModels  The list of scoutable models, each model must use `Laravel\Scout\Searchable` trait.
+     *                                     The order of the models will be the order of the results.
+     *                                     Example: [`\App\Models\Book::class`, `\App\Models\Serie::class`, `\App\Models\Author::class`].
      */
-    public function getRelevantResults(): array
+    public static function make(?string $query, array $scoutableModels): self
     {
-        /** @var Collection <string,mixed> */
-        $list = collect();
+        $self = new self($query, $scoutableModels);
 
-        /** @var string $model */
-        foreach ($this->results as $model => $results) {
-            /** @var AnonymousResourceCollection $collection */
-            $collection = $results;
-            $json = $collection->toJson();
-            $results_list = json_decode($json);
-            $relevant_results = array_splice($results_list, 0, $this->top_limit);
-
-            $list->put($model, [
-                'relevant' => $relevant_results,
-                'other' => $results_list,
-            ]);
-        }
-
-        return $list->toArray();
+        return $self;
     }
 
     /**
-     * Search Entity[].
+     * Set the limit of results by model
      */
-    private function search(): SearchEngine
+    public function limit(?int $limit): self
     {
-        $items = ClassParserService::toCollection(app_path('Models'));
-
-        foreach ($items as $item) {
-            $this->entitySearch($item);
-        }
+        $this->limit = $limit;
 
         return $this;
     }
 
-    private function entitySearch(ClassParserItem $item): void
+    /**
+     * Execute the search.
+     */
+    public function get(): self
     {
-        $reflect = $item->getReflect();
-        $static = $reflect->getName();
-        $name = $reflect->getShortName();
-        $key = Str::plural($name);
+        $this->results = $this->search();
 
-        $slug = preg_split('/(?=[A-Z])/', $name);
-        $slug = implode('-', $slug);
-        $key = Str::plural(Str::slug($slug));
+        return $this;
+    }
 
-        if (in_array($key, $this->types)) {
-            $this->results[$key] =
-                $static::search($this->q) // @phpstan-ignore-line
-                    ->get();
+    /**
+     * Get the results.
+     *
+     * @return Collection<string, Collection<Model>>
+     */
+    public function getResults(): Collection
+    {
+        return $this->results;
+    }
+
+    /**
+     * Get the count of results.
+     */
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * Get the limit of results.
+     */
+    public function getLimit(): ?int
+    {
+        return $this->limit;
+    }
+
+    /**
+     * Get the query.
+     */
+    public function getQuery(): ?string
+    {
+        return $this->query;
+    }
+
+    public function toArray(bool $flatten = false): array
+    {
+        if ($flatten) {
+            return $this->results->flatten()->toArray();
         }
+
+        return $this->results->toArray();
+    }
+
+    public function toJson(bool $flatten = false): string
+    {
+        return json_encode($this->toArray($flatten));
+    }
+
+    /**
+     * Transform the results to a `Illuminate\Http\Resources\Json\JsonResource`.
+     */
+    public function toResource(string $resource, bool $flatten = false): array
+    {
+        $instance = new $resource(collect());
+        if (! $instance instanceof JsonResource) {
+            throw new \InvalidArgumentException('The resource must be an instance of '.JsonResource::class);
+        }
+
+        $this->resource = $resource;
+
+        /** @var Collection<string, JsonResource> */
+        $resources = collect();
+
+        // Transform the results to resources.
+        foreach ($this->results as $class => $collect) {
+            $resources->put($class, $this->resource::collection($collect));
+        }
+
+        if ($flatten) {
+            /** @var Collection<JsonResource> */
+            $items = collect();
+
+            // Merge the resources.
+            foreach ($resources as $resource) {
+                $items->push(...$resource->toArray(request()));
+            }
+
+            return $items->toArray();
+        }
+
+        return $resources->toArray();
+
+    }
+
+    /**
+     * Search for models.
+     */
+    private function search(): Collection
+    {
+        if (! $this->query) {
+            // No query, no search.
+            return collect();
+        }
+
+        if (empty($this->scoutable)) {
+            // No scoutable models, no search.
+            return collect();
+        }
+
+        /** @var Collection<string, Collection<Model>> */
+        $results = collect();
+
+        // Search for each scoutable model.
+        foreach ($this->scoutable as $class) {
+            if (method_exists($class, 'search')) {
+                $builder = $class::search($this->query);
+                $results->put($class, $builder->get());
+            } else {
+                throw new \InvalidArgumentException('The model '.$class.' must use Laravel\Scout\Searchable trait');
+            }
+        }
+
+        // Rename the keys of the results.
+        foreach ($results as $class => $collect) {
+            $modelName = $this->modelName($class);
+            $results->put($modelName, $collect);
+            $results->forget($class);
+        }
+
+        // Limit the results.
+        if ($this->limit) {
+            $results = $results->map(function ($collect) {
+                return $collect->take($this->limit);
+            });
+        }
+
+        // Count the results.
+        foreach ($results as $collect) {
+            $collect->each(function () {
+                $this->count++;
+            });
+        }
+
+        return $results;
+    }
+
+    /**
+     * Get the model name.
+     */
+    private function modelName(string $class): string
+    {
+        $instance = new $class();
+        $class = new ReflectionClass($instance);
+        $name = $class->getShortName();
+
+        return Str::plural($name);
     }
 }
