27c914c18811c49beeb680429dc1a3b84924681e
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-07-29 10:54:34 +0200

    Refactor TerminalService.php to replace multiple spaces with a single arrow symbol in the action string

--- src/Services/TerminalService.php
+++ src/Services/TerminalService.php
@@ -117,7 +117,7 @@ class TerminalService
                 $spaces,
                 preg_replace('#({[^}]+})#', '<fg=yellow>$1</>', $uri),
                 $dots,
-                str_replace('   ', ' › ', $action ?? ''),
+                str_replace('   ', ' › ', $action),
             ), $this->verbose && ! empty($middleware) ? "<fg=#6C7280>$middleware</>" : null];
         })
             ->flatten()
